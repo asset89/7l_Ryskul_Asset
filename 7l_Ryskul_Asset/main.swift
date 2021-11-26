de73baef674d505c43c1a3c0ed0944e664caab71
asset89 <asset89@gmail.com>
2021-11-26 21:07:05 +0600

    Add BookShop

--- 7l_Ryskul_Asset/main.swift
+++ 7l_Ryskul_Asset/main.swift
@@ -5,7 +5,8 @@
 //  Created by Asset Ryskul on 26.11.2021.
 //
 
-import Foundation
 
-print("Hello, World!")
 
+let bookShop = BookShop()
+print(bookShop.sell(itemNamed: "50 grays of shadow")?.name)
+print(bookShop.sell(itemNamed: "War & Piece")?.name)
